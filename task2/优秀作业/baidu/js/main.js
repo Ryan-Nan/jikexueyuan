b04b96d061af9f8534ff426c3ab3209e172983e2
Ryan <yangnan1311@foxmail.com>
2017-03-07 22:31:15 +0800

    html5 and CSS3

---
+++ task2/优秀作业/baidu/js/main.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var baidu={
+	init: function(){
+		this.baidusetting();
+	},
+	
+	baidusetting: function(){
+		$('#navMore, #moreProduct').mouseover(function(){
+			$("#moreProduct").show();
+		}).mouseout(function(){
+			$("#moreProduct").hide();
+		})
+	}
+			
+	}
+	
+$(function(){
+	baidu.init();
+})
